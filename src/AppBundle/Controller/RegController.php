3f33cec8dcf9703994332d6bc8e382c479042a4f
Diana <israelyan.diana@mail.ru>
2016-03-04 13:01:31 +0400

    Reg

---
+++ src/AppBundle/Controller/RegController.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class MainController extends Controller
+{
+    /**
+     * @Route("/index", name="index")
+     */
+    public function indexAction()
+    {
+        return new Response("Hello my friend");
+    }
+}
